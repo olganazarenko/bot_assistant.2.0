01124a8616d13f9a1e85b5b6fea4cf3c8ea7db72
olganazarenko <113229061+olganazarenko@users.noreply.github.com>
2022-10-25 21:45:46 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -2,41 +2,58 @@ from collections import UserDict
 
 
 class Field:
-    def __init__(self, name):
-        self.value = name
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+    
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.value = value
+    #     check_phone(value)
+
+    @staticmethod
+    def check_phone(phone: str) -> str:
+        pattern = r"(^380|0|80)\d{9}$"
+        match = re.fullmatch(pattern, phone)
+        if not match:
+            raise ValueError("Invalid, please enter a valid phone number")
+
+        return phone
+
+
+class Record:
+    def __init__(self, name, phone=None):
+        self.name = Name(name)
+        self.phones = [Phone(phone)] if phone else []
+
+    def add_phone(self, phone):
+        phone = Phone(phone)
+
+        if any(x.value == phone.value for x in self.phones):
+            return self.phones.append(phone)
+        return phone
+
+    def delete_phone(self, phone):
+        for phone in self.phone:
+            if phone.value == phone:
+                self.phone.remove(phone)
+                return phone
+
+    def update_phone(self, old_phone, new_phone):
+        for phone in self.phone:
+            if phone.value == old_phone:
+                self.update(new_phone)
+                return new_phone
 
 
 class AddressBook(UserDict):
 
     def add_record(self, record):
-        self.data[record.name.value] = record
 
-
-class Name(Field):
-    pass
-
-
-class Phone(Field):
-    pass
-
-
-class Record(Field):
-    def __init__(self, name):
-        self.name = Name(name)
-        self.phones = []
-
-    def add_phone(self, phone):
-        self.phones.append(Phone(phone))
-
-    def change_phone(self, old_phone, new_phone):
-        for phone in self.phones:
-            if phone.value == old_phone:
-                self.add_phone(new_phone)
-                self.phones.remove(phone)
-                return True
-
-    def delete_phone(self, new_phone):
-        for phone in self.phones:
-            if phone.value == new_phone:
-                self.phones.remove(phone)
-                return True
+        contact = Record(name=Name, phone=Phone)
+        self.data[record.name.value] = contact
